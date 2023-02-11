f303003d2cdfad112c54ad45d3094f5af0baebd1
Yusril Hasan <79882296+wahyubudii@users.noreply.github.com>
2023-02-11 17:42:43 +0700

    fix development

--- index.js
+++ index.js
@@ -5,13 +5,20 @@ import mongoose from "mongoose";
 import router from "./src/routes/userRoutes";
 import blogRouter from "./src/routes/blogRoutes";
 
+// port
 const port = 5000;
 
+// rest object
 const app = express();
+
+// middleware
 app.use(express.json());
+
+// routes
 app.use("/api/user", router);
 app.use("/api/blog", blogRouter);
 
+// databse connection
 mongoose.set("strictQuery", true);
 mongoose
   .connect(process.env.MONGO_URL)
